554dbd5e64669fa6d168bebea71cdc9ea96e9aa9
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-10-14 12:54:07 +0600

    added regestation and login operation. works perfectly.

--- Hackathon-Project-Competition-App/src/app/pages/register/register.ts
+++ Hackathon-Project-Competition-App/src/app/pages/register/register.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
+
+  isLoginFormVisible = signal<boolean>(false);
+  http = inject(HttpClient);
+  router = inject(Router)
+
+  registerObj: any = {
+  "fullName": "",
+  "email": "",
+  "password": "",
+  "collegeName": "",
+  "role": ""
+}
+loginObj: any= {
+  "email": "",
+  "password": ""
+}
+  
+  toggleForm() {
+    this.isLoginFormVisible.set(!this.isLoginFormVisible())
+  }
+  onRegister() {
+    const vallueCheck = this.registerObj;
+    debugger;
+    this.http.post("https://api.freeprojectapi.com/api/ProjectCompetition/register",this.registerObj).subscribe(
+      {
+        next:(result)=>{
+          debugger
+          alert("Registration Success")
+      },
+      error:(error)=>{
+        debugger
+        alert(error.error)
+      }
+    }
+  )
+  }
+  onLogin(){
+    debugger;
+    this.http.post("https://api.freeprojectapi.com/api/ProjectCompetition/login",this.loginObj).subscribe(
+      {
+        next:(result)=>{
+          debugger
+          localStorage.setItem('hackathonuser', JSON.stringify(result));
+          this.router.navigateByUrl('/home')
+          alert("Login Success")
+      },
+      error:(error)=>{
+        debugger
+        alert(error.error)
+      }
+    }
+  )
+  }
 
 }
